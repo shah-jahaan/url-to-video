3c3be8a486645dce5f4f5d203dcce95e1b8a2584
Shah Jahan <shahjahan.me.official@gmail.com>
2025-01-04 10:06:35 -0800

    added backend

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import uniqid from 'uniqid';
+import fs from 'fs';
+import cors from 'cors';
+import { GPTScript } from '@gptscript-ai/gptscript';
+import { RunEventType } from '@gptscript-ai/gptscript';
+
+const gptscript = new GPTScript();
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/create-story', async (req, res) => {
+    const url = req.query.url;
+    const dir = uniqid();
+    const path = './stories/'+dir;
+    fs.mkdirSync(path, {recursive: true});
+    console.log({
+        url,
+    });
+    const run = await gptscript.run('./story.gpt', {
+        input: `--url ${url} --dir ${path}`,
+        disableCache: true,
+    });
+    run.on(RunEventType.Event, ev => {
+        if (ev.type === RunEventType.CallFinish && ev.output) {
+            console.log(ev.output);
+        }
+    });
+    const result = await run.text();
+    res.json(result);
+});
+
+app.listen(8080, () => {
+    console.log(`Listening on port 8080`);
+});
